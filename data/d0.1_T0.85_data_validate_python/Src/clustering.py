8a79f9a48723fef9003a2ec30ed1b53142520df2
Vishal Soni <jayesh.s@iitgn.ac.in>
2023-02-28 05:55:10 +0530

    clustering changes

--- data/d0.1_T0.85_data_validate_python/Src/clustering.py
+++ data/d0.1_T0.85_data_validate_python/Src/clustering.py
@@ -13,19 +13,24 @@ class clustering:
         self.fileSaver = fileSaver()
         self.L = 10
         pass
-    def assign_cluster(self, process_no, file_start, file_end, r_cut):
+    def assign_cluster(self, process_no, file_start, file_end, r_cut):  
+        frequencies = []
         for i in range(file_start, file_end + 1):
             filePath = f"XYZ_{process_no}_{i}"
             df = self.statHelp.dat_to_dataframe(filePath)
             x = list(df[:][1])
             y = list(df[:][2])
             z = list(df[:][3])
-            self.cluster(x, y, z, r_cut, process_no, i)
+            frequencies = self.cluster(x, y, z, r_cut, process_no, i, frequencies)
+        plt.bar(np.arange(len(frequencies) + 1), frequencies)
+        plt.savefig(f'freq_{process_no}_{file_start}_{file_end}.png')
 
-    def cluster(self, x, y, z, r_cut, process_no, file_no):
+    def cluster(self, x, y, z, r_cut, process_no, file_no, frequencies):
         n = len(x)
         cluster_id = 0
         cluster_assignments = {}
+        if len(frequencies) == 0:
+            frequencies = np.zeros(n)
         assigned = np.full(n, False, dtype=bool)
         for i in range(n):
             if assigned[i]:
@@ -40,6 +45,9 @@ class clustering:
                 if dis <= r_cut:
                     cluster_assignments[cluster_id].append(j)
                     assigned[j] = True
+        for point_list in cluster_assignments.values():
+            currsize = len(point_list)
+            frequencies[currsize] += 1
         mat = []
         mat2 = []
         for c_id in range(1, cluster_id + 1):
@@ -52,6 +60,7 @@ class clustering:
         # df = pd.DataFrame(mat2, columns = ['pid', 'cid'])
         self.fileSaver.save_file('clustered', 'c_XYZ', '.dat', [process_no, file_no], mat)
         self.fileSaver.save_file('clustered', 'c_pid', '.dat', [process_no, file_no], mat2)
+        return frequencies
 
     def distance(self, p1, p2):
         a = np.abs(p1[0] - p2[0])
